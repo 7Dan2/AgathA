a132392ebdad9bf222c0c6bb0422f3e625b31f20
7dan2 <unic.norton@gmail.com>
2020-08-06 12:08:18 +0200

    real initial starting point

--- src/App.js
+++ src/App.js
@@ -14,31 +14,18 @@ function App() {
       
       <header className="App-header">
         <div>
-        <img src="http://dailydropcap.com/images/A-7-cap.png" title="An other simple generator" align="left" alt="A"/>
+        {/* <img src="http://dailydropcap.com/images/A-7-cap.png" title="An other simple generator" align="left" alt="A"/> */}
         <img src="http://dailydropcap.com/images/A-6-cap.png" title="Daily Drop Cap by Jessica Hische" align="left" alt="A"/>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </div>
 
-        
         <StartButton />
-        
-        
-        <div>
-          <MainForm />
-        </div>
-        
         
         </header>
         </div>
         
-        
-        
-      
-
-    
-  );
+    );
   }
 }
-
 
 export default App;
--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -2,22 +2,13 @@ import React from 'react';
 import './MainForm.css'
 
 const MainForm = () =>
-    <table>
-        <th>Test</th>
-        <th>Test1</th>
-        <th><button>Add</button></th>
-
-        <tr>
-            <td>1</td>
-            <td>11</td>
-            <td><button>Add</button></td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>21</td>
-            <td><input></input></td>
-        </tr>
-    </table>
+    {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
  
 
 export default MainForm;
